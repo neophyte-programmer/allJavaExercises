eb4c9032c9e84c68a7b2c8bcde36ae9e3c34d6d8
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-10-11 20:52:27 +0000

    Update

---
+++ Exercise 4 - While Loops/src/app2.java
@@ -0,0 +1,15 @@
+//Write a program to print numbers from 1 to 10.
+public class app2 {
+
+	public static void main(String[] args) {
+		int number = 1;
+		
+		while (number <= 10)
+		{
+			System.out.println(number);
+			number++;
+		}
+
+	}
+
+}
