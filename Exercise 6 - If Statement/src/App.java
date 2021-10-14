7b834fe0b3c512818c20d6a7b43a1c9613c6ff67
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-10-14 12:15:12 +0000

    Update

---
+++ Exercise 6 - If Statement/src/App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		int num1 = 4;
+		int num2 = 5;
+		
+		if (num1 == num2) {
+			System.out.println("Yes, it is true");
+		}
+		else if(num1 > num2) {
+			System.out.println("The first number is greater than the second");
+		}
+		else {
+			System.out.println("The first number is less than the second");
+		}
+
+	}
+
+}
